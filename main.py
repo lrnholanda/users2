4624fe188d16806e26819d3907231e180705794f
lrnholanda <lrnholanda@gmail.com>
2021-03-13 21:16:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+current_users = ['lore', 'ju', 'carlos', 'chris', 'italo']
+new_users = ['fernanda', 'mateus', 'ju', 'joão', 'lore']
+
+for user in new_users:
+  if user in current_users:
+    print("Enter a new usermame")
+  else:
+    print("usermame avaible")  
